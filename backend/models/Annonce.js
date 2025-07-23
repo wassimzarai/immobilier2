b6583358c12c99b7eb8413b150746c0af6563e29
Zarai Wassim <wassimzarai28@gmail.com>
2025-07-23 13:53:57 +0100

    annonce details

--- backend/models/Annonce.js
+++ backend/models/Annonce.js
@@ -19,7 +19,45 @@ const AnnonceSchema = new mongoose.Schema(
     description: { type: String, required: true, trim: true, maxlength: 2000 },
     photos: { type: [String], validate: [v => v.length <= 10, 'Limite de 10 photos.'] },
     auteur: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
-    estPubliee: { type: Boolean, default: true }
+    estPubliee: { type: Boolean, default: true },
+    surfaceConstruite: { type: Number },
+    annees: { type: String },
+    typeSol: { type: String },
+    etage: { type: Number },
+    orientation: { type: String },
+    pieces: { type: Number },
+    chambres: { type: Number },
+    sallesDeBains: { type: Number },
+    caracteristiques: {
+      jardin: { type: Boolean, default: false },
+      terrasse: { type: Boolean, default: false },
+      garage: { type: Boolean, default: false },
+      ascenseur: { type: Boolean, default: false },
+      vueSurMer: { type: Boolean, default: false },
+      vueSurMontagnes: { type: Boolean, default: false },
+      piscine: { type: Boolean, default: false },
+      concierge: { type: Boolean, default: false },
+      chambreRangement: { type: Boolean, default: false },
+      meuble: { type: Boolean, default: false }
+    },
+    facadeExterieure: { type: String },
+    interieur: {
+      salonEuropeen: { type: Boolean, default: false },
+      antenneParabolique: { type: Boolean, default: false },
+      cheminee: { type: Boolean, default: false },
+      climatisation: { type: Boolean, default: false },
+      chauffageCentral: { type: Boolean, default: false },
+      securite: { type: Boolean, default: false },
+      doubleVitrage: { type: Boolean, default: false },
+      porteBlindee: { type: Boolean, default: false }
+    },
+    optionsSupplementaires: {
+      cuisineEquipee: { type: Boolean, default: false },
+      refrigerateur: { type: Boolean, default: false },
+      four: { type: Boolean, default: false },
+      machineALaver: { type: Boolean, default: false },
+      microOndes: { type: Boolean, default: false }
+    }
   },
   { timestamps: true }
 );
